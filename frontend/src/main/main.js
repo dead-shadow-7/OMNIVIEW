4a76bb8889d0d9a8fde0fcc21e9440bcaaf6ef84
dead-shadow-7 <deadshadow12119@gmail.com>
2026-04-08 16:13:27 +0530

    Hide menu bar by default and fix monitoring UI overlay/panel issues

--- frontend/src/main/main.js
+++ frontend/src/main/main.js
@@ -148,6 +148,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
